4d670e3545c7062c862ea2e0dc3565db90d53df0
Aidan Gardner <gardnera3@wit.edu>
2021-12-01 14:59:01 -0500

    menu updates

--- src/javafyClient.java
+++ src/javafyClient.java
@@ -80,95 +80,211 @@ public class javafyClient //implements Runnable
 		 }
 	}
 //	 
-	 public static void back() {//Plays the last song played
-		 Queue<Song> tempQueue = new LinkedList<Song>();
-		 Song p = prevQueue.poll();
-		  
-		 if(p == null){
-			 System.out.println("No previous tracks in queue");
-			 System.exit(1);
-		 }
-		 
-		 tempQueue.add(p);
-		 tempQueue.addAll(trackQueue);
-		 
-		 trackQueue.clear();
-		 trackQueue.addAll(tempQueue);
-	}
+//	 public static void back() {//Plays the last song played
+//		 Queue<Song> tempQueue = new LinkedList<Song>();
+//		 Song p = prevQueue.poll();
+//		  
+//		 if(p == null){
+//			 System.out.println("No previous tracks in queue");
+//			 System.exit(1);
+//		 }
+//		 
+//		 tempQueue.add(p);
+//		 tempQueue.addAll(trackQueue);
+//		 
+//		 trackQueue.clear();
+//		 trackQueue.addAll(tempQueue);
+//	}
 //	 
 //	 public static void restart() {
 //		 //Starts the current song from the beginning
 //	}
 //	 
-//	 public static void return() {//most likely type will change to song instead of void.
-//		 //Shows the previous display [returns error if display is at start]
-//		 
-//	}
-//	 
-//	 public static void select(List playlist){
-//	 //Shows the contents of the specified playlist or album
-//	}
-//	 
-	public static void show(){//Shows the queue
-		int tQ = trackQueue.size();
-		Queue<Song> tempQueue = new LinkedList<Song>();
-		
-		for(int i = 0; i <= tQ; i++){
-			Song s = trackQueue.poll();
-			tempQueue.add(s);
-			System.out.printf("%d", s);
-		}
+	 public static void back(){//most likely type will change to song instead of void.
+		 //Shows the previous display [returns error if display is at start]
+		 String lvl = getCurrentLevel();
+		 
+		 switch(lvl) {
+			case "topLevel":
+				setCurrentLevel("topLevel");
+				System.out.print("You're already at the home page and con't go back any further...");
+				home();
+			break;
+			case "playlistLevel":
+				setCurrentLevel("topLevel");
+				home();
+			break;
+			case "albumLevel":
+				setCurrentLevel("topLevel");
+				home();
+			break;
+			case "songLevel":
+				setCurrentLevel("topLevel");
+				home();
+			break;
+			case "playlist1":
+				setCurrentLevel("playlistLevel");
+				playlistLevel();
+			break;
+			case "playlist2":
+				setCurrentLevel("playlistLevel");
+				playlistLevel();
+			break;
+			case "album1":
+				setCurrentLevel("albumLevel");
+				albumLevel();
+			break;
+			case "album2":
+				setCurrentLevel("albumLevel");
+				albumLevel();
+			break;
+			}
+		 
 	}
+	 
+public static void select(String command){
+ //Shows the contents of the specified playlist or album
+	 String lvl = getCurrentLevel();
+	 
+	 switch(lvl) {
+		case "topLevel":
+
+			switch (command) {
+			case "1/select":
+				setCurrentLevel("playlistLevel");
+				playlistLevel();
+				break;
+			case "2/select":
+				setCurrentLevel("albumLevel");
+				albumLevel();
+				break;
+			case "3/select":
+				setCurrentLevel("songLevel");
+				songLevel();
+				break;
+			default:
+				System.out.println("Ivalid input: " + command);
+				System.out.println("Please enter a valid number (1-3)\"/return\"");
+			break;
+			}
+		break;
+		case "playlistLevel":
+			switch (command) {
+			case "1/select":
+				setCurrentLevel("playlist1");
+				playlist1();
+				break;
+			case "2/select":
+				setCurrentLevel("playlist2");
+				playlist2();
+				break;
+			}
+		break;
+		case "albumLevel":
+			switch (command) {
+			case "1/select":
+				setCurrentLevel("album1");
+				album1();
+				break;
+			case "2/select":
+				setCurrentLevel("album2");
+				album2();
+				break;
+			}
+			break;
+		}
+}
+
+//	 
+//	public static void show(){//Shows the queue
+//		int tQ = trackQueue.size();
+//		Queue<Song> tempQueue = new LinkedList<Song>();
+//		
+//		for(int i = 0; i <= tQ; i++){
+//			Song s = trackQueue.poll();
+//			tempQueue.add(s);
+//			System.out.printf("%d", s);
+//		}
+//	}
+
+//goes on the end of anything that is not a select or return function/ method (refresh)
+public static void refresh(){
+	 String lvl = getCurrentLevel();
+	 
+	 switch(lvl) {
+		case "topLevel":
+			setCurrentLevel("topLevel");
+			home();
+		break;
+		case "playlistLevel":
+			setCurrentLevel("playlistLevel");
+			playlistLevel();
+		break;
+		case "albumLevel":
+			setCurrentLevel("albumLevel");
+			albumLevel();
+		break;
+		case "songLevel":
+			setCurrentLevel("songLevel");
+			songLevel();
+		break;
+		case "playlist1":
+			setCurrentLevel("playlist1");
+			playlist1();
+		break;
+		case "playlist2":
+			setCurrentLevel("playlist2");
+			playlist2();
+		break;
+		case "album1":
+			setCurrentLevel("album1");
+			album1();
+		break;
+		case "album2":
+			setCurrentLevel("album2");
+			album2();
+		break;
+		}
+
+}
 	
 	private static String currentLevel;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Start of main
 	public static void main(String[] args) {
-	    currentSong = "Music/mmad.wav";
-	    audioPlayer player = new audioPlayer(currentSong);
-	    Application.launch(audioPlayer.class,args);
+	 //   currentSong = "Music/mmad.wav";
+	   // audioPlayer player = new audioPlayer(currentSong);
+	  //  Application.launch(audioPlayer.class,args);
 			Scanner sc = new Scanner(System.in);
 			//keeps track of current level for return command
 			//topLevel, playlistLevel, albumLevel, songLevel
 			setCurrentLevel("topLevel");
 			//playlist / album / song selection level
-			int topLevel = 0;
 			
 			
 			System.out.println("Welcome to Javafy!");
-			System.out.println("input the number of which menu you'd prefer to open:");
+			System.out.println("use #/select to choose");
 			
-			//make method later for return statement to call!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 			while (getCurrentLevel().equals("topLevel")){
 				setCurrentLevel("topLevel");
 				
 				System.out.println("[1]Playlists:		[2]Albums:							[3]Songs:");
-				int in1 = sc.nextInt();
-				if (in1 == 1||in1 == 2||in1 == 3) {
-				topLevel = in1;
+				String in1 = sc.nextLine();
+				commandTaker(in1);
 				}
-				else {
-				System.out.println("Ivalid input: " + in1);
-				System.out.println("Please enter a valid number (1-3)");
-				}
-			
-			
+	}
 	
-			switch (topLevel) {
-			case 1:
-				setCurrentLevel("playlistLevel");
-				playlistLevel();
-				break;
-			case 2:
-				currentLevel = "albumLevel";
-				albumLevel();
-				break;
-			case 3:
-				currentLevel = "songLevel";
-				songLevel();
-				break;
+//calls top level
+	public static void home(){
+		Scanner sc = new Scanner(System.in);
+		while (getCurrentLevel().equals("topLevel")){
+			setCurrentLevel("topLevel");
+			
+			System.out.println("[1]Playlists:		[2]Albums:							[3]Songs:");
+			String in1 = sc.nextLine();
+			commandTaker(in1);
 			}
 		}
-	}
-		
+//calls playlists
 	public static void playlistLevel(){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("[1]playlist 1");
@@ -179,138 +295,127 @@ public class javafyClient //implements Runnable
 		
 		String playlistCommand = sc.nextLine();
 		
-		switch(playlistCommand) {
-		case "1/select":
-			playlist1();
-		break;
-		case "2/select":
-			playlist2();
-		break;
-		case "/return":
-			setCurrentLevel("TopLevel");
-			break;
-		default:
 			commandTaker(playlistCommand);
-			playlistCommand = sc.nextLine();
-			playlistLevel();
-		break;
-		}
-	}
 
+	}
+//playlist 1 menu
 	public static void playlist1(){
 		setCurrentLevel("playlist1");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("[1]Feels like summer - Childish Gambino");
-		System.out.println("[2]Summertime magic - Childish Gambino");
-		System.out.println("[3]Once in a Lifetime - Talking Heads");
+		System.out.println("[1]Summertime magic - Childish Gambino");
+		System.out.println("[2]MOUNTAIN - Monsune");
+		System.out.println("[3]JADE - Monsune");
 		System.out.println("");
 		System.out.println("To view the list of commands for interacting with the playlist's songs type \"/help\"");
 		
 		String playlistSongCommand = sc.nextLine();
 		
-		switch(playlistSongCommand) {
-		case "/return":
-			setCurrentLevel("playlistLevel");
-		break;
-		default:
 			commandTaker(playlistSongCommand);
-			playlist1();
-		break;
-		}
-	}
 
+	}
+//playlist 2 menu
 	public static void playlist2() {
 			setCurrentLevel("playlist2");
 			Scanner sc = new Scanner(System.in);
 			System.out.println("[1]Feels like summer - Childish Gambino");
-			System.out.println("[2]Summertime magic - Childish Gambino");
+			System.out.println("[2]CLOUD - Monsune");
+			System.out.println("[3]OUTTA MY MIND - Monsune");
+			System.out.println("[4]1998 - Monsune");
 			System.out.println("");
 			System.out.println("To view the list of commands for interacting with the playlist's songs type \"/help\"");
 			
 			String playlistSongCommand = sc.nextLine();
 			
-			switch(playlistSongCommand) {
-			case "/return":
-				setCurrentLevel("playlistLevel");
-			break;
-			default:
-				commandTaker(playlistSongCommand);
-				playlist2();
-			break;
-			}
+			commandTaker(playlistSongCommand);
 	}
-
+//album menu
 	public static void albumLevel() {
 		setCurrentLevel("albumLevel");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("[1]Summer Pack - Childish Gambino");
-		System.out.println("[2]");
+		System.out.println("[2]Tradition - Monsune");
 		System.out.println("");
 		System.out.println("To view the contents of the album you want to view enter the album #/select");
 		System.out.println("To view the list of commands for interacting with the album type \"/help\"");
 		
 		String albumCommand = sc.nextLine();
-		
-		switch(albumCommand) {
-		case "1/select":
-			album1();
-		break;
-		case "2/select":
-			album2();
-		break;
-		case "/return":
-			setCurrentLevel("topLevel");
-		break;
-		default:
 			commandTaker(albumCommand);
-			albumCommand = sc.nextLine();
-		break;
+	}
+	//album 2 menu
+		public static void album2() {
+			setCurrentLevel("album2");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("[1]1998 - Monsune");
+			System.out.println("[2]CLOUD - Monsune");
+			System.out.println("[3]OUTTA MY MIND - Monsune");
+			System.out.println("[4]MOUNTAIN - Monsune");
+			System.out.println("[5]JADE - Monsune");
+			System.out.println("");
+			System.out.println("To view the list of commands for interacting with the album type \"/help\"");
+			
+			String albumCommand = sc.nextLine();
+				commandTaker(albumCommand);
+			
 		}
-	}
-
-	private static void album1() {
-		// TODO Auto-generated method stub
+//album 1 menu
+	public static void album1() {
+		setCurrentLevel("album1");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("[1]Feels like summer - Childish Gambino");
+		System.out.println("[2]Summertime magic - Childish Gambino");
+		System.out.println("");
+		System.out.println("To view the list of commands for interacting with the album type \"/help\"");
+		
+		String albumCommand = sc.nextLine();
+			commandTaker(albumCommand);
 		
 	}
 
-	private static void album2() {
-		// TODO Auto-generated method stub
-		
-	}
-
+//song menu / picker
 	public static void songLevel() {
 		setCurrentLevel("songLevel");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("[1]Feels like summer - Childish Gambino");
 		System.out.println("[2]Summertime magic - Childish Gambino");
 		System.out.println("[3]Once in a Lifetime - Talking Heads");
+		System.out.println("[4]1998 - Monsune");
+		System.out.println("[5]CLOUD - Monsune");
+		System.out.println("[6]OUTTA MY MIND - Monsune");
+		System.out.println("[7]MOUNTAIN - Monsune");
+		System.out.println("[8]JADE - Monsune");
 		System.out.println("");
 		System.out.println("To view the list of commands for interacting with the album type \"/help\"");
 		
-		String albumCommand = sc.nextLine();
-		
-		switch(albumCommand) {
-		case "/return":
-			setCurrentLevel("topLevel");
-		break;
-		default:
-			commandTaker(albumCommand);
-			albumCommand = sc.nextLine();
-		break;
-		}
+		String songCommand = sc.nextLine();
+			commandTaker(songCommand);
 	}
 
 
-	//redirects commands
+//redirects commands
 	public static void commandTaker(String command){
-		if (command.equals("/help")) {
+		switch(command) {
+		case "1/select":
+			select(command);
+		break;
+		case "2/select":
+			select(command);
+		break;
+		case "3/select":
+			select(command);
+		break;
+		case "/return":
+			back();
+		break;
+		case "/help":
 			help();
-			}
-		
-		if (command.equals("/")) {
-			//commandmethod();
-			}
+		break;
+		default:
+			System.out.println("error: enter valid command");
+			refresh();
+		break;
 		}
+	}
+		
 		
 	public static void help(){
 		System.out.println("\"/help\" - Shows list of commands");
